8a7964e78939c6271c05d0a6c9bd22971c19a374
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-04-17 23:35:35 -0300

    chore: adiciona exemplo para modo desenvolvimento

--- functions/src/routes.js
+++ functions/src/routes.js
@@ -3,6 +3,26 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+// modo desenvolvimento usar .env
+
+const credenciais = {
+  "type": process.env.APP_TYPE,
+  "project_id": process.env.APP_PROJECT_ID,
+  "private_key_id": process.env.APP_PRIVATE_KEY_ID,
+  "private_key": process.env.APP_PRIVATE_KEY,
+  "client_email": process.env.APP_CLIENT_EMAIL,
+  "client_id": process.env.APP_CLIENT_ID,
+  "auth_uri": process.env.APP_AUTH_URI,
+  "token_uri": process.env.APP_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.APP_AUTH_PROVIDER_CERT,
+  "client_x509_cert_url": process.env.APP_CLIENT_CERT
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse),
+  databaseURL: process.env.APP_DATABASE_URL,
+});
+
 const db = admin.database();
 
 const routes = new router()
